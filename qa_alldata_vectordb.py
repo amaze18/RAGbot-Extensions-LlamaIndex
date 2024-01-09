c22baf9b112b7c48a8abfb0468720e95717086a8
Koosh0610 <119339573+Koosh0610@users.noreply.github.com>
2024-01-09 08:33:38 +0530

    Update qa_alldata_vectordb.py

--- qa_alldata_vectordb.py
+++ qa_alldata_vectordb.py
@@ -4,7 +4,7 @@ import os
 import faiss
 import warnings
 warnings.filterwarnings("ignore")
-openai.api_key = os.environ['SECRET_TOKEN']
+openai.api_key = "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"
 import langchain
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain.retrievers import MultiQueryRetriever
@@ -13,6 +13,7 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain_community.embeddings.openai import OpenAIEmbeddings
+from sqlalchemy.exc import InvalidRequestError
 from langchain_community.vectorstores import FAISS
 from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
@@ -25,7 +26,7 @@ from langchain.chains import RetrievalQA
 import re
 text_folder_path = r"scraped_files/processed"
 texts=[]
-"""
+
 for file in os.listdir(text_folder_path):
     try:
         with open(text_folder_path+ "/" + file, "r", encoding="UTF-8") as f:
@@ -43,21 +44,18 @@ for file in os.listdir(text_folder_path):
         f.close()
 
 text_loader_kwargs={'autodetect_encoding': True}
-loader=DirectoryLoader(text_folder_path,glob="./*.txt",use_multithreading=True,loader_kwargs=text_loader_kwargs,silent_errors=True)
+loader=DirectoryLoader(text_folder_path,glob="./*.txt",loader_cls=UnstructuredFileLoader,use_multithreading=True,loader_kwargs=text_loader_kwargs,silent_errors=True)
 documents=loader.load()
-text_splitter=RecursiveCharacterTextSplitter(chunk_size=1200,chunk_overlap=120)
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=2000,chunk_overlap=2000)
 texts=text_splitter.split_documents(documents)
-print(texts)
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002",openai_api_key=openai.api_key)
 db=FAISS.from_documents(texts,embeddings)
-"""
+db.save_local("scraped_files/processed")
 
 def chat_gpt(question):
 
     embeddings= OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=openai.api_key)
-
-
-
+    db=FAISS.load_local("scraped_files/processed",embeddings)
     retriever = db.as_retriever(search_type='similarity', search_kwargs={"k": 3} )#do not increase k beyond 3, else
     llm = OpenAI(model='text-embedding-ada-002',temperature=0, openai_api_key=openai.api_key)
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
@@ -66,23 +64,23 @@ def chat_gpt(question):
     query = question
     res = qa(query)
     try:
-	    
-	    response = openai.ChatCompletion.create(
-		model="gpt-3.5-turbo",
-		#model="gpt-4-0613",
-		messages=[
-	    	{"role": "system", "content": "You are a helpful chatbot who answers questions asked based only on context provided in a friendly tone, if you do not know the answer, say I don\'t know"},
-	    	{"role": "user", "content": f"{res}"}
-		])
-	    answer= response["choices"][0]["message"]["content"]
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        #model="gpt-4-0613",
+        messages=[
+            {"role": "system", "content": "You are a helpful chatbot who answers questions asked based only on context provided in a friendly tone, if you do not know the answer, say I don\'t know"},
+            {"role": "user", "content": f"{res}"}
+        ])
+        answer= response["choices"][0]["message"]["content"]
     except InvalidRequestError:
-	    response = openai.ChatCompletion.create(
-		model="gpt-3.5-turbo",
-		max_tokens=2000,
-		#model="gpt-4-0613",
-		messages=[
-	    	{"role": "system", "content": "You are a helpful chatbot who answers questions asked based only on context provided in a friendly tone, if you do not know the answer, say I don\'t know"},
-	    	{"role": "user", "content": f"{res}"}
-		])
-	    answer= response["choices"][0]["message"]["content"]
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=2000,
+        #model="gpt-4-0613",
+        messages=[
+            {"role": "system", "content": "You are a helpful chatbot who answers questions asked based only on context provided in a friendly tone, if you do not know the answer, say I don\'t know"},
+            {"role": "user", "content": f"{res}"}
+        ])
+        answer= response["choices"][0]["message"]["content"]
+
     return answer
