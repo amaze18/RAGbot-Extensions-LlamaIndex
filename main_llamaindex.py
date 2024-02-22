a22197273f8c265cdf641adc576c092502c26e18
Koosh0610 <119339573+Koosh0610@users.noreply.github.com>
2024-02-22 07:31:07 +0530

    updated code

--- main_llamaindex.py
+++ main_llamaindex.py
@@ -1,44 +1,40 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
 import streamlit as st
-from datetime import timedelta
-from llama_index.memory import ChatMemoryBuffer
+
+import os
+
 import openai
-from create_context import answer_question
+
+import pandas as pd
 from PIL import Image
-from llama_index.retrievers import BM25Retriever
 import boto3
 from io import StringIO
-#from hugchat import hugchat
-#from hugchat.login import Login
-from llama_index.schema import MetadataMode
 from rouge import Rouge
-import os
-from llama_index.postprocessor import SentenceTransformerRerank
-from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
-from htbuilder.units import percent, px
-from htbuilder.funcs import rgba, rgb
-from qa_llamaindex import indexgenerator
-from llama_index.llms import OpenAI
-from llama_index import ServiceContext
-from llama_index.query_engine import RetrieverQueryEngine
-from llama_index import (get_response_synthesizer)
-
-
-
-from llama_index.query_engine import RetrieverQueryEngine
-
-from llama_index.chat_engine import CondensePlusContextChatEngine
-# import QueryBundle
-
-# Retrievers
-from llama_index.retrievers import (BaseRetriever)
-
-from typing import List
-#from llama_index import (VectorStoreIndex,SimpleDirectoryReader)
 import tiktoken
+
+from htbuilder import HtmlElement, div, br, hr, a, p, img, styles
+from htbuilder.units import percent, px
+
+from qa_llamaindex import indexgenerator
+from create_context import answer_question
+
+from llama_index.legacy.memory import ChatMemoryBuffer
+from llama_index.legacy.retrievers import BM25Retriever
+from llama_index.legacy.retrievers import VectorIndexRetriever
+from llama_index.legacy.schema import QueryBundle
+from llama_index.legacy.schema import MetadataMode
+
+from llama_index.legacy.postprocessor import LongContextReorder 
+from qa_llamaindex import indexgenerator
+from llama_index.legacy.llms import OpenAI
+from llama_index.legacy import ServiceContext
+from llama_index.legacy.query_engine import RetrieverQueryEngine
+from llama_index.legacy.query_engine import RetrieverQueryEngine
+from llama_index.legacy.chat_engine import CondensePlusContextChatEngine
+from llama_index.legacy.retrievers import BaseRetriever
+
 def image(src_as_string, **style):
     return img(src=src_as_string, style=styles(**style))
 
@@ -115,33 +111,36 @@ with st.sidebar:
     image = Image.open('Ivlogo.png.png')
     st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
 
-        #storage_context = StorageContext.from_defaults(persist_dir=indexPath)
-        #index = load_index_from_storage(storage_context)
-indexPath=r"/home/ec2-user/BITSPilani/llamaindex_entities_0.2/1024/text_embedding_ada_002"
-documentsPath=r"llamaindex_entities_0.2\1024\text_embedding_ada_002"
-index=indexgenerator(indexPath,documentsPath)
-vector_retriever = index.as_retriever(similarity_top_k=2)
 #nodes=index.docstore.docs.values()
-bm25_retriever = BM25Retriever.from_defaults(index=index, similarity_top_k=2)
+
+indexPath_2000=r"index\2000\text_embedding_ada_002"
+documentsPath_2000=r"Text_Files_Old"
+index_2000=indexgenerator(indexPath_2000,documentsPath_2000)
+vector_retriever_2000 = VectorIndexRetriever(index=index_2000,similarity_top_k=2)
+bm25_retriever_2000 = BM25Retriever.from_defaults(index=index_2000, similarity_top_k=2)
+postprocessor = LongContextReorder()
+
 class HybridRetriever(BaseRetriever):
-    def __init__(self, vector_retriever, bm25_retriever):
-        self.vector_retriever = vector_retriever
-        self.bm25_retriever = bm25_retriever
+    def __init__(self,vector_retriever_2000, bm25_retriever_2000):
+        #self.vector_retriever_1000 = vector_retriever_1000
+        #self.bm25_retriever_1000 = bm25_retriever_1000
+        self.vector_retriever_2000 = vector_retriever_2000
+        self.bm25_retriever_2000 = bm25_retriever_2000
         super().__init__()
 
     def _retrieve(self, query, **kwargs):
-        bm25_nodes = self.bm25_retriever.retrieve(query, **kwargs)
-        vector_nodes = self.vector_retriever.retrieve(query, **kwargs)
-
-        # combine the two lists of nodes
-        all_nodes = []
-        node_ids = set()
-        for n in bm25_nodes + vector_nodes:
-            if n.node.node_id not in node_ids: #apply processing at this very stage
-                all_nodes.append(n)
-                node_ids.add(n.node.node_id)
+        bm25_nodes_2000 = self.bm25_retriever_2000.retrieve(query, **kwargs)
+        vector_nodes_2000 = self.vector_retriever_2000.retrieve(query, **kwargs)
+        encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        context_str = "\n\n".join([n.node.get_content(metadata_mode=MetadataMode.LLM).strip() for n in bm25_nodes_2000+vector_nodes_2000])
+        num_token =  len(encoding.encode(context_str))
+        if num_token > 3900:
+            all_nodes = postprocessor.postprocess_nodes(nodes=bm25_nodes_2000+vector_nodes_2000,query_bundle=QueryBundle(query_str=prompt.lower()))
+        else:
+            all_nodes = bm25_nodes_2000+vector_nodes_2000
         return all_nodes
-hybrid_retriever = HybridRetriever(vector_retriever, bm25_retriever)
+hybrid_retriever=HybridRetriever(vector_retriever_2000,bm25_retriever_2000)
+
 # User-provided prompt
 page_bg_img = '''
 <style>
@@ -151,97 +150,85 @@ background-size: cover;
 }
 </style>
 '''
-
-def callback():
-    st.session_state['form_clicked'] = True
-if "messages" not in st.session_state.keys():
-        st.session_state.messages = [{"role": "assistant", "content": "Ask anything about I-Venture @ ISB ..."}]
-    # Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-context_prompt=(
-        "You are a helpful and friendly chatbot who addresses queries regarding I-Venture @ ISB."
-        "Here are the relevant documents for the context:\n"
-        "{context_str}"
-        "\nInstruction 1: Use the previous chat history, or the context above to answer. Be concise."
-        "\nInstruction 2: Say I don't know if you do not find the answer in context provided."
-        )
-
 memory=ChatMemoryBuffer.from_defaults(token_limit=3900)
 rouge = Rouge()
+
+context_prompt=(
+        "You are a helpful and friendly chatbot who addresses queries in detail regarding I-Venture @ ISB."
+        "Here are the relevant documents for the context:\n"
+        "{context_str}"
+        "\nInstruction: Use the previous chat history above and context, to interact and help the user. Never give any kinds of links, email addresses or contact numbers in the answer."
+        )
+def get_response(prompt):
+    llm = OpenAI(model="gpt-3.5-turbo")
+    service_context = ServiceContext.from_defaults(llm=llm)
+    query_engine=RetrieverQueryEngine.from_args(retriever=hybrid_retriever,service_context=service_context,verbose=True)
+    chat_engine=CondensePlusContextChatEngine.from_defaults(query_engine,memory=memory,context_prompt=context_prompt)
+    nodes = hybrid_retriever.retrieve(prompt.lower())
+    response = chat_engine.chat(str(prompt.lower()))
+    validating_prompt = ("""You are an intelligent bot designed to assist users on an organization's website by answering their queries. You'll be given a user's question and an associated answer. Your task is to determine if the provided answer effectively resolves the query. If the answer is unsatisfactory, return 0.\n
+                        Query: {question}  
+                        Answer: {answer}
+                        Your Feedback:
+                        """)
+    feedback = llm.complete(validating_prompt.format(question=prompt,answer=response.response))
+    if feedback.text==str(0):
+        st.write("DISTANCE APPROACH")
+        response , joined_text=answer_question(prompt.lower())
+        scores = rouge.get_scores(response, joined_text)
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message)     
+        response_list = [response, prompt , scores]  
+        df = pd.read_csv('logs/conversation_log.csv')
+        new_row = {'Question': str(prompt), 'Answer': response,'Rouge_Score' : scores}
+        df = pd.concat([df, pd.DataFrame(new_row, index=[0])], ignore_index=True)
+        df.to_csv('logs/conversation_logs.csv', index=False)
+        bucket = 'aiex' # already created on S3
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer)
+        s3_resource= boto3.resource('s3',aws_access_key_id=os.environ["ACCESS_ID"],aws_secret_access_key= os.environ["ACCESS_KEY"])
+        s3_resource.Object(bucket, 'conversation_log.csv').put(Body=csv_buffer.getvalue())
+        return response_list                                     
+    else:
+        context_str = "\n\n".join([n.node.get_content(metadata_mode=MetadataMode.LLM).strip() for n in nodes])
+        scores=rouge.get_scores(response.response,context_str)
+        message = {"role": "assistant", "content": response.response}
+        st.session_state.messages.append(message)
+        response_list = [response.response , prompt , scores]
+        df = pd.read_csv('logs/conversation_logs.csv')
+        new_row = {'Question': str(prompt), 'Answer': response.response,'Rouge_Score' : scores}
+        df = pd.concat([df, pd.DataFrame(new_row, index=[0])], ignore_index=True)
+        df.to_csv('logs/conversation_log.csv', index=False)
+        bucket = 'aiex' # already created on S3
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer)
+        s3_resource= boto3.resource('s3',aws_access_key_id=os.environ['ACCESS_ID'],aws_secret_access_key= os.environ['ACCESS_KEY'])
+        s3_resource.Object(bucket, 'conversation_log.csv').put(Body=csv_buffer.getvalue())
+        return response_list
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [{"role": "assistant", "content": "Let me know about your grievance!"}]
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+# If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-                        llm = OpenAI(model="gpt-3.5-turbo")
-                        service_context = ServiceContext.from_defaults(llm=llm)
-                        query_engine=RetrieverQueryEngine.from_args(retriever=hybrid_retriever,service_context=service_context)
-                        chat_engine=CondensePlusContextChatEngine.from_defaults(query_engine,memory=memory,context_prompt=context_prompt)
-                        nodes = hybrid_retriever.retrieve(str(prompt))
-                        context_str = "\n\n".join([n.node.get_content(metadata_mode=MetadataMode.LLM).strip() for n in nodes])
-                        response = chat_engine.chat(str(prompt))
-                        validating_prompt = ("""You are an intelligent bot designed to assist users on an organization's website by answering their queries. You'll be given a user's question and an associated answer. Your task is to determine if the provided answer effectively resolves the query. If the answer is unsatisfactory, return 0.\n
-Query: {question}  
-Answer: {answer}
-Your Feedback:
-""")
-                        feedback = llm.complete(validating_prompt.format(question=prompt,answer=response.response))
-                        if feedback.text==str(0):
-                            st.write("DISTANCE APPROACH")
-                            response , joined_text=answer_question(prompt)
-                            st.write(response)
-                            message = {"role": "assistant", "content": response}
-                            st.session_state.messages.append(message)
-                            if 'form_clicked' not in st.session_state:
-                                st.session_state['form_clicked'] = False
-                            if not st.session_state['form_clicked']:
-                                    with st.form('Answer Feedback'):
-                                        answer_quality = st.slider(label='Rate the answer on a scale of 5. ', min_value=0.0, max_value=5.0, step=0.5)
-                                        expected_answer = st.text_area('Enter expected answer: ')
-                                        scores = rouge.get_scores(response, joined_text)
-                                        submitted = st.form_submit_button(label="Submit",on_click=submitted)
-                            if st.session_state['form_clicked']:
-                                            df = pd.read_csv('logs/conversation_log.csv')
-                                            new_row = {'question': str(prompt), 'answer': response, 'Answer Quality' : answer_quality, 'Expected Answer': expected_answer,'Rouge_Score' : scores}
-                                            df = pd.concat([df, pd.DataFrame(new_row, index=[0])], ignore_index=True)
-                                            df.to_csv('logs/conversation_log.csv', index=False)
-                                            bucket = 'aiex' # already created on S3
-                                            csv_buffer = StringIO()
-                                            df.to_csv(csv_buffer)
-                                            s3_resource= boto3.resource('s3',aws_access_key_id=os.environ["ACCESS_ID"],aws_secret_access_key= os.environ["ACCESS_KEY"])
-                                            s3_resource.Object(bucket, 'conversation_log.csv').put(Body=csv_buffer.getvalue())
-                                            reset()
-                        else:
-                            st.write(response.response)
-                            message = {"role": "assistant", "content": response.response}
-                            st.session_state.messages.append(message)
-                            if 'form_clicked' not in st.session_state:
-                                st.session_state['form_clicked'] = False
-                            if not st.session_state['form_clicked']:
-                                    with st.form('Answer Feedback'):
-                                        answer_quality = st.slider(label='Rate the answer on a scale of 5. ', min_value=0.0, max_value=5.0, step=0.5)
-                                        expected_answer = st.text_area('Enter expected answer: ')
-                                        scores = rouge.get_scores(response, context_str)
-                                        submitted = st.form_submit_button(label="Submit",on_click=callback)
-                                        if st.session_state['form_clicked']:
-                                                        df = pd.read_csv('logs/conversation_log.csv')
-                                                        new_row = {'question': str(prompt), 'answer': response, 'Answer Quality' : answer_quality, 'Expected Answer': expected_answer,'Rouge_Score' : scores}
-                                                        df = pd.concat([df, pd.DataFrame(new_row, index=[0])], ignore_index=True)
-                                                        df.to_csv('logs/conversation_log.csv', index=False)
-                                                        bucket = 'aiex' # already created on S3
-                                                        csv_buffer = StringIO()
-                                                        df.to_csv(csv_buffer)
-                                                        s3_resource= boto3.resource('s3',aws_access_key_id=os.environ["ACCESS_ID"],aws_secret_access_key= os.environ["ACCESS_KEY"])
-                                                        s3_resource.Object(bucket, 'conversation_log.csv').put(Body=csv_buffer.getvalue())
-                                        
+            response = get_response(prompt)
+            st.write(response[0])
+            message = {"role": "assistant", "content": response[0]}
+            st.session_state.messages.append(message) # Add response to message history
 
+
+        
+
+
+                                #s3_resource= boto3.resource('s3',aws_access_key_id=os.environ["ACCESS_ID"],aws_secret_access_key= os.environ["ACCESS_KEY"])
+                            #s3_resource.Object(bucket, 'conversation_log.csv').put(Body=csv_buffer.getvalue())
+                        
 myargs = [
     "Made in India",""
     " with ❤️ by ",
