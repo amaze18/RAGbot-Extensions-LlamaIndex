584f4959587b2db0c98b346f6a7d1ad33da51883
Koosh0610 <119339573+Koosh0610@users.noreply.github.com>
2024-02-22 16:47:58 +0530

    Update qa_llamaindex.py

--- qa_llamaindex.py
+++ qa_llamaindex.py
@@ -1,15 +1,14 @@
 import os
 import openai
 openai.api_key=os.environ['SECRET_TOKEN']
-from llama_index import SimpleDirectoryReader
-from llama_index.extractors.metadata_extractors import EntityExtractor
-from llama_index.node_parser import SentenceSplitter
-from llama_index.ingestion import IngestionPipeline
-from llama_index import ServiceContext, VectorStoreIndex
-from llama_index.llms import OpenAI
+from llama_index.legacy import SimpleDirectoryReader
+from llama_index.legacy.extractors.metadata_extractors import EntityExtractor
+from llama_index.legacy.node_parser import SentenceSplitter
+from llama_index.legacy.ingestion import IngestionPipeline
+from llama_index.legacy import ServiceContext, VectorStoreIndex
+from llama_index.legacy.llms import OpenAI
 import streamlit as st 
-from llama_index import (StorageContext,load_index_from_storage)
-from llama_index.memory import ChatMemoryBuffer
+from llama_index.legacy import (StorageContext,load_index_from_storage)
 @st.cache_resource
 def indexgenerator(indexPath, documentsPath):
 
@@ -40,60 +39,6 @@ def indexgenerator(indexPath, documentsPath):
         #load existing index
         print("Existing")
         storage_context = StorageContext.from_defaults(persist_dir=indexPath)
-        index = load_index_from_storage(storage_context)
+        index = load_index_from_storage(storage_context,service_context=ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0, system_prompt="You are an expert on I-Venture @ ISB and your job is to answer technical questions. Assume that all questions are related to I-Venture @ ISB. Keep your answers technical and based on facts – do not hallucinate features.")))
         
     return index
-
-def react_chatbot_engine(index):
-
-    #memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
-    chat_engine = index.as_chat_engine(
-    chat_mode="react",
-    #memory=memory,
-    system_prompt=(
-        "You are a helpful and friendly chatbot who addresses questions regarding I-Venture @ ISB politely."
-        ),
-    verbose=True,
-    )
-    return chat_engine
-
-def condense_question_chatbot_engine(index):
-
-    memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
-    chat_engine = index.as_chat_engine(
-    chat_mode="condense_question",
-    memory=memory,
-    #system_prompt=(
-       # "You are a helpful and friendly chatbot who addresses queries regarding I-Venture @ ISB."
-        #),
-    verbose=True,
-    )
-    return chat_engine
-
-def condense_context_question_chatbot_engine(index):
-
-    memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
-    chat_engine = index.as_chat_engine(
-    chat_mode="condense_plus_context",
-    memory=memory,
-    system_prompt=(
-        "You are a helpful and friendly chatbot who addresses queries regarding I-Venture @ ISB."
-        "Here are the relevant documents for the context:\n"
-        "{context_str}"
-        "\nInstruction: Use the previous chat history, or the context above, to interact and help the user."
-        ),
-    verbose=True,
-    )
-    return chat_engine
-
-def context_chatbot_engine(index):
-
-    memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
-    chat_engine = index.as_chat_engine(
-    chat_mode="context",
-    memory=memory,
-    system_prompt=(
-        "You are a helpful and friendly chatbot who addresses queries regarding I-Venture @ ISB."
-        ),
-    )
-    return chat_engine
